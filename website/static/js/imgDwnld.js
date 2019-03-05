cac66208b7c7cae718332517064108dbebab2e76
root <root@DESKTOP-D50AFIR.localdomain>
2019-03-05 17:23:09 -0500

    Cleaning up image download library

--- website/static/js/imgDwnld.js
+++ website/static/js/imgDwnld.js
@@ -1,10 +1,6 @@
 function addDownloadButtons(){
     jQuery(function($){
         $(document).ready(function () {
-            //Remove pipes, commas and 'by' from the meta
-            //Move image above title and meta
-            //Add a container to the featured image on archive pages
-            console.log("pagerdy");
             $(".downloadable").each(function() {
                 $(this).wrapAll('<a class="downloadImgLink" href="'+this.src+'" download="'+getName(this)+'"><div class="image"></div></a>');
                 console.log("loopreached");
@@ -16,9 +12,6 @@ function addDownloadButtons(){
 function getName(imageFile) {
     var fullPath = imageFile.src;
     var filename = fullPath.replace(/^.*[\\\/]/, '');
-    // or, try this, 
-    // var filename = fullPath.split("/").pop();
-
    return filename;
 }
 addDownloadButtons();
